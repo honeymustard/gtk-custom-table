6a58f15ce8e8b0f4d416db4235631aad6bd5a627
Adrian Solumsmo <adrian.solumsmo@gmail.com>
2014-11-01 13:38:49 +0100

    updated example to include an actual table

--- examples/example-01.c
+++ examples/example-01.c
@@ -6,23 +6,45 @@ int main(int argc, char *argv[]) {
 
     gtk_init(&argc, &argv);
 
-    printf("Gtk Custom Table\n");
-
-    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Examples");
-
-    GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
-    //gtk_box_pack_start(GTK_BOX(vbox), mwin->menu, FALSE, FALSE, 0);
-
     int widths[] = {
-        10, 10, 10, 10
+        100, -1, -1, 100
     };
 
-    gtk_custom_table_new(4, 4, 200, 200, widths);
+    /* initiate widgets */
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+    GtkWidget *table = gtk_custom_table_new(4, 80, widths);
+    GtkWidget *scroll = gtk_scrolled_window_new(NULL, NULL);
+    GtkWidget *viewport = gtk_viewport_new(NULL, NULL);
+
+    /* add containers */
+    gtk_container_add(GTK_CONTAINER(viewport), table);
+    gtk_container_add(GTK_CONTAINER(scroll), viewport);
+    gtk_box_pack_start(GTK_BOX(vbox), scroll, TRUE, TRUE, 0);
+    gtk_container_add(GTK_CONTAINER(window), vbox);
+
+    /* set cell text */
+    int i = 0;
+    char temp[10];
+
+    for(i = 0; i < gtk_custom_table_get_rows(table); i++) {
+
+        sprintf(temp, "%d", i + 1);
+        gtk_custom_table_set_cell_text(table, 0, i, temp);
+        sprintf(temp, "%d", (i + 1) * 25);
+        gtk_custom_table_set_cell_text(table, 1, i, temp);
+    }
+
+    /* set meta */
+    gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scroll), 
+        GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+    gtk_viewport_set_shadow_type(GTK_VIEWPORT(viewport), GTK_SHADOW_NONE);
+
+    /* add signals */
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-    /* last steps, show window */
-    gtk_container_add(GTK_CONTAINER(window), vbox);
+    /* show window */
+    gtk_window_set_title(GTK_WINDOW(window), "Examples");
     gtk_window_set_default_size(GTK_WINDOW(window), 500, 400);
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
     gtk_widget_show_all(window);
